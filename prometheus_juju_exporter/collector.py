1db68383d9e223034feb42319c71594aac849042
Martin Kalcok <69471063+mkalcok@users.noreply.github.com>
2023-03-17 23:08:49 +0100

    Add feature to skip certain interfaces when detecting HW type (#35)
    * Add feature to skip certain interfaces when detecting HW type
    
    * Better explanation for `skip_interfaces` option
    
    Co-authored-by: Andrea Ieri <aieri@users.noreply.github.com>
    
    * Simplify regexp matching interface names
    
    ---------
    
    Co-authored-by: Andrea Ieri <aieri@users.noreply.github.com>

--- prometheus_juju_exporter/collector.py
+++ prometheus_juju_exporter/collector.py
@@ -1,4 +1,5 @@
 """Collector module."""
+import re
 from enum import Enum
 from logging import getLogger
 from typing import Any, Dict, List
@@ -122,20 +123,34 @@ class Collector:
         for label in stale_labels.values():
             self.data[gauge_name]["labelvalues_remove"].append(list(label[0].values()))
 
-    def _get_machine_type(self, machine: Dict) -> MachineType:
+    def _get_machine_type(self, machine: Dict, machine_id: str) -> MachineType:
         """Detect machine type based on its MAC address.
 
         :param dict machine: status information for a machine
         :return MachineType: MachineType class object indicating the machine type
         """
-        interfaces = machine["network-interfaces"].values()
+        self.logger.debug("Determining type of machine: %s", machine_id)
+        machine_type = MachineType.METAL
+        skip_interfaces = self.config["detection"]["skip_interfaces"].as_str_seq()
+        interface_filter = re.compile(r"|".join(skip_interfaces))
 
-        for i in interfaces:
-            mac_address = i["mac-address"]
+        for interface, properties in machine["network-interfaces"].items():
+            if skip_interfaces and interface_filter.search(interface) is not None:
+                self.logger.debug(
+                    "Disregarding interface %s. Matching filter: %s",
+                    interface,
+                    skip_interfaces,
+                )
+                continue
+
+            mac_address = properties["mac-address"]
+            self.logger.debug("Considering interface %s with MAC: %s", interface, mac_address)
             if mac_address.startswith(tuple(self.virt_mac_prefixes)):
-                return MachineType.KVM
+                machine_type = MachineType.KVM
+                break
 
-        return MachineType.METAL
+        self.logger.debug("Machine %s is of type: %s", machine_id, machine_type)
+        return machine_type
 
     def _get_host_identifier(self, host: Dict) -> str:
         """Try to find a valid identifier for the host.
@@ -187,11 +202,11 @@ class Collector:
         :param str gauge_name: the name of the gauge
         """
         for machine in machines.values():
-            machine_type = self._get_machine_type(machine)
             value = self._get_gauge_value(status=machine["agent-status"]["status"])
             machine_id = self._get_host_identifier(machine)
 
             if machine_id != "None":
+                machine_type = self._get_machine_type(machine, machine_id)
                 labels = self._create_gauge_label(
                     hostname=machine_id,
                     model_name=model_name,
